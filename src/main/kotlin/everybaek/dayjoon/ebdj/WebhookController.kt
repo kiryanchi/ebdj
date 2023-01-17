339c3bbbe4c374be58cc943394c05a6e7397d05c
kiryanchi <kiryanchi@naver.com>
2023-01-17 15:46:20 +0900

    Controller & Service

--- src/main/kotlin/everybaek/dayjoon/ebdj/WebhookController.kt
+++ src/main/kotlin/everybaek/dayjoon/ebdj/WebhookController.kt
@@ -1,4 +1,16 @@
 package everybaek.dayjoon.ebdj
 
-class WebhookController {
+import everybaek.dayjoon.ebdj.dto.GithubWebhookPushPayload
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WebhookController(
+    val webhookService: WebhookService,
+) {
+    @PostMapping("/api/webhook/push")
+    fun push(@RequestBody payload: GithubWebhookPushPayload) {
+        this.webhookService.push(payload)
+    }
 }
--- src/main/kotlin/everybaek/dayjoon/ebdj/WebhookService.kt
+++ src/main/kotlin/everybaek/dayjoon/ebdj/WebhookService.kt
@@ -1,2 +1,23 @@
-package everybaek.dayjoon.ebdj 
+package everybaek.dayjoon.ebdj
 
+import everybaek.dayjoon.ebdj.dto.GithubWebhookPushPayload
+import org.springframework.stereotype.Service
+
+@Service
+class WebhookService {
+
+    fun push(payload: GithubWebhookPushPayload) {
+        val repository = payload.repository
+        val headCommit = payload.headCommit
+
+        ifPrivateRepositoryThrowException(repository.isPrivate)
+
+        
+    }
+
+    private fun ifPrivateRepositoryThrowException(isPrivate: Boolean) {
+        // TODO("RepositoryPrivateException")
+        if (isPrivate)
+            IllegalArgumentException("Repository must be public")
+    }
+}
