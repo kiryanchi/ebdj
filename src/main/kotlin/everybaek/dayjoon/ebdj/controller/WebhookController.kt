6f89027760a00558dd625f201dd3dc32d01d798e
kiryanchi <kiryanchi@naver.com>
2023-02-01 01:16:57 +0900

    WebhookController log

--- src/main/kotlin/everybaek/dayjoon/ebdj/controller/WebhookController.kt
+++ src/main/kotlin/everybaek/dayjoon/ebdj/controller/WebhookController.kt
@@ -2,6 +2,7 @@ package everybaek.dayjoon.ebdj.controller
 
 import everybaek.dayjoon.ebdj.domain.dto.GithubWebhookPushPayload
 import everybaek.dayjoon.ebdj.service.WebhookService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 class WebhookController(
     val webhookService: WebhookService,
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @PostMapping("/api/webhook/push")
     fun push(@RequestBody payload: GithubWebhookPushPayload) {
-        println(payload)
+        log.debug("새로운 payload push API 요청 들어옴")
+        log.debug("payload: $payload")
         this.webhookService.push(payload)
     }
 }
